0e1f03358d9a1a076f9bbfd1a6dccb0807aaaf10
Sateeshreddy N <satishreddyn579@gmail.com>
2021-11-01 13:23:45 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Weather
+
+A description of this package.
+
+This is used to get the Weather report based on City 
---
+++ Sources/Weather/Weather.swift
@@ -0,0 +1,109 @@
+import Foundation
+import SwiftyJSON
+
+public class Weather {
+    public var city: String
+    public var condition: String?
+    public var temperature: String?
+    private let baseURL = URL(string: "https://www.metaweather.com/api/location/")
+    private let dispatchGroup = DispatchGroup()
+    
+    public init(city: String) {
+        self.city = city.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? city.lowercased()
+        let _ = try? getCurrentConditions()
+    }
+    
+    internal func fetchJSON(path: String, completion: @escaping (Result<JSON, Error>) -> Void) {
+        enum fetchJSONError: Error {
+            case invalidURL
+            case missingData
+        }
+        
+        guard let url = URL(string: path, relativeTo: baseURL) else {
+            completion(.failure(fetchJSONError.invalidURL))
+            return
+        }
+        
+        let dataTask = URLSession.shared.dataTask(with: url) { (data, urlResponse, error) in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(fetchJSONError.missingData))
+                return
+            }
+            
+            do {
+                let jsonResult = try JSON(data: data)
+                completion(.success(jsonResult))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        dataTask.resume()
+    }
+    
+    internal func getLocationId() throws -> Int? {
+        var locationId: Int?
+        var fetchError: Error?
+        
+        dispatchGroup.enter()
+        fetchJSON(path: "search/?query=\(self.city)") { result in
+            switch result {
+            case .success(let json):
+                locationId = json[0]["woeid"].int
+                self.dispatchGroup.leave()
+            case .failure(let error):
+                print("Request failed with error: \(error)")
+                locationId = nil
+                fetchError = error
+                self.dispatchGroup.leave()
+            }
+        }
+        
+        dispatchGroup.wait()
+        if let fetchError = fetchError {
+            throw fetchError
+        }
+        return locationId
+    }
+    
+    public func getCurrentConditions() throws -> [String: String]? {
+        var weatherInfo: [String: String] = [:]
+        var fetchError: Error?
+        
+        guard let locationId = try getLocationId() else {
+            return nil
+        }
+        
+        dispatchGroup.enter()
+        fetchJSON(path: "\(locationId)") { [weak self] result in
+            switch result {
+            case .success(let json):
+                let consolidatedWeatherInfo = json["consolidated_weather"][0]
+                weatherInfo["condition"] = consolidatedWeatherInfo["weather_state_name"].string
+                weatherInfo["temperature"] = "\(consolidatedWeatherInfo["the_temp"].doubleValue) °C"
+                self?.condition = weatherInfo["condition"]
+                self?.temperature = weatherInfo["temperature"]
+                self?.dispatchGroup.leave()
+            case .failure(let error):
+                fetchError = error
+                self?.dispatchGroup.leave()
+            }
+        }
+        
+        dispatchGroup.wait()
+        if let fetchError = fetchError {
+            throw fetchError
+        }
+        return weatherInfo
+    }
+    
+    internal func convertCelToFar(celsiusTemp: Double) -> Int {
+        return Int((celsiusTemp * 9/5)) + 32
+        
+    }
+}
